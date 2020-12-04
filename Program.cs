a599d36175e7fd347e3543a9d5be9e53334bcfbd
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-12-04 09:58:27 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
